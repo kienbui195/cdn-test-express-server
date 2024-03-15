7b0778cf4afaa141fb7c8687e47abbff441b73bd
Kien Bui <kaybi9x@gmail.com>
2024-03-15 17:50:48 +0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
-const post = 3000
+const post = 3000;
 
-app.use(cors())
+const cssFilePath = path.join(__dirname, "/static/styles.css");
+const jsFilePath = path.join(__dirname, "/static/event.js");
+
+app.use(cors());
+
+app.get("/static/style.css", async (req, res) => {
+  try {
+    // Đọc nội dung của tệp tin CSS
+    const cssContent = await fs.readFile(cssFilePath, "utf-8");
+
+    // Trả về nội dung của tệp tin CSS như một phản hồi
+    res.setHeader("Content-Type", "text/css");
+    res.send(cssContent);
+  } catch (error) {
+    console.error("Error reading CSS file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/static/javascript.js", async (req, res) => {
+  try {
+    // Đọc nội dung của tệp tin CSS
+    const jsContent = await fs.readFile(jsFilePath, "utf-8");
+
+    // Trả về nội dung của tệp tin CSS như một phản hồi
+    res.setHeader("Content-Type", "application/javascript");
+    res.send(jsContent);
+  } catch (error) {
+    console.error("Error reading CSS file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.get('/', (req, res) => {
-  res.send("Ok")
+  res.send("OK")
 })
 
 app.listen(post, (req, res) => {
   console.log(`Server listening on ${post}`);
-})
+});
